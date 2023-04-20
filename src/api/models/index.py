d65b5e56f9fcdf4c0214c376e6da55a01071d55e
Eduardo Andrade <edu2andrade@gmail.com>
2023-04-20 17:44:34 +0000

    waiting for users

--- src/api/models/index.py
+++ src/api/models/index.py
@@ -1,3 +1,5 @@
 from api.models.db import db
 from api.models.services import Services
 from api.models.servicesWorkers import ServicesWorkers
+from api.models.company import Company
+from api.models.workers import Workers
