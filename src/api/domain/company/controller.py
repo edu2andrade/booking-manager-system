4bec5dac0ce60c30a6c3ea60b5e1c960a14cb05c
Enricrypto <eibarraf@gmail.com>
2023-04-29 09:39:35 +0000

    fix:only admins can update company

--- src/api/domain/company/controller.py
+++ src/api/domain/company/controller.py
@@ -2,6 +2,9 @@ import api.domain.company.repository as Repository
 import api.utilities.handle_response as Response
 
 def create_company(data):
+    if data['user_id'] is None or data['user_id'] == '':
+        return Response.response_error('User ID not found', 400)
+
     if data['cif'] is None or data['cif'] == '':
         return Response.response_error('Company CIF not found', 400)
     
@@ -9,6 +12,10 @@ def create_company(data):
         return Response.response_error('Company name not found', 400)
     
     return Repository.create_company(data), 201
+
+def get_company_list():
+	all_companies = Repository.get_company_list()
+	return Response.response_ok('List of all companies', all_companies)
 
 def get_company_by_id(company_id):
     company = Repository.get_company_by_id(company_id)
@@ -18,17 +25,18 @@ def get_company_by_id(company_id):
     return company
 
 def update_company(update_company, company_id):
+    
     update_company = Repository.update_company(update_company, company_id)
-    if update_company:
-        return Response.response_ok(f'User with id: {company_id}, has been updated in database.', update_company.serialize())
-    else:
-        return Response.response_error(f'User with id: {company_id}, not found in database.', 404)
 
-def delete_company(id):
-    if not isinstance(id, int):
-        return Response.response_error("ID is not a number", 404)
-    company = Repository.delete_company(id) 
-    if company is not None:
-        return Response.response_ok("Company deleted", 200) 
+    if update_company:
+        return Response.response_ok(f'Company with id: {company_id}, has been updated in database.', update_company.serialize())
     else:
-        return Response.response_error("ID not found", 404)
+        return Response.response_error(f'Company with id: {company_id}, not found in database.', 404)
+
+def delete_company(company_id):
+    
+    is_deleted_company = Repository.delete_company(company_id) 
+    
+    if is_deleted_company:
+        return Response.response_ok(f'Company with id: {company_id}, has been deleted from database', 200)
+    return Response.response_error(f'Company with id: {company_id}, has not been found in database.', 400)
--- src/api/domain/company/repository.py
+++ src/api/domain/company/repository.py
@@ -1,35 +1,38 @@
 from api.models.index import db, Company
 
+
 def create_company(data):
     new_company = Company(data['user_id'], data['cif'], data['name'], data['description'], data['address'], data['working_schedule'])
     db.session.add(new_company)
     db.session.commit()
     return new_company.serialize()
 
+def get_company_list():
+    all_companies = Company.query.all()
+    serialized_companies = list(map(lambda company: company.serialize(), all_companies))
+    return serialized_companies
+
 def get_company_by_id(id):
     company = Company.query.get(id)
     return company.serialize()
 
-def update_company(data, company_id):
+def update_company(update_company, company_id):
     company = Company.query.get(company_id)
     if company:
-        company.user_id = data['user_id']
-        company.cif = data['cif']
-        company.name = data['name']
-        company.description = data['description']
-        company.address = data['address']
-        company.working_schedule = data['working_schedule']
+        company.name = update_company['name']
+        company.description = update_company['description']
+        company.address = update_company['address']
+        company.working_schedule = update_company['working_schedule']
         db.session.commit()
         return company
     else:
         return None
 
-def delete_company(id):
-    company = Company.query.get(id)
-    if company is None:  
-        return company  
-    else:
-        #add delete functions for services, workers and products 
+def delete_company(company_id):
+    company = Company.query.get(company_id)
+    if company is not None:  
         db.session.delete(company)
         db.session.commit()
-    return company
+    else:
+        #add delete functions for services, workers and products 
+        return company  
