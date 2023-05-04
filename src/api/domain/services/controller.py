931b9db976e6aac121c304595f45b23ce07058cc
Eduardo Andrade <edu2andrade@gmail.com>
2023-05-04 05:26:13 +0000

    fix: conditional logic changes, now verifies if company exists and user have id = 1 in routes that need token

--- src/api/domain/services/controller.py
+++ src/api/domain/services/controller.py
@@ -6,15 +6,15 @@ def create_new_service(company_id, current_user_id, body):
     if company is None:
         return {'msg': f'The Company with id: {company_id}, do not exists in this database.', 'status': 404}
 
-    if current_user_id != company.user_id:
+    if current_user_id == company.user_id and current_user_id == 1:
+        return Repository.create_new_service(body, company_id)
+    else:
         return {'msg': 'You do not have rights to create new services!', 'status': 403}
-
-    return Repository.create_new_service(body, company_id)
 
 def get_services_by_company(company_id):
     company = Company.query.get(company_id)
     if company is None:
-        return {'msg': f'The Company with id: {company_id}, do not exists in this database.', 'status': 404}
+        return {'msg': f'The Company with id: {company_id}, does not exists in this database.', 'status': 404}
 
     services = Repository.get_services_by_company(company_id)
     return services
@@ -22,21 +22,25 @@ def get_services_by_company(company_id):
 def get_single_service(service_id):
     service = Repository.get_single_service(service_id)
     if service is None:
-        return {'msg': f'Service with id: {service_id}, do not exists in this database.', 'status': 404}
+        return {'msg': f'The service with id: {service_id}, does not exists in this database.', 'status': 404}
 
     return service
 
 def delete_service(service_id, current_user_id):
     service = Services.query.get(service_id)
     if service is None:
-        return {'msg': f'Service with id: {service_id}, do not exists in this database.', 'status': 404}
+        return {'msg': f'The service with id: {service_id}, does not exists in this database.', 'status': 404}
         
     service_company_id = service.company_id
     company = Company.query.get(service_company_id)
+
+    if company is None:
+        return {'msg': f'The Company with id: {service_company_id}, does not exists in this database.', 'status': 404}
+
     company_user_id = company.user_id
 
-    if current_user_id != company_user_id:
+    if current_user_id == company_user_id and current_user_id == 1:
+        deleted_service = Repository.delete_service(service_id)
+        return deleted_service
+    else:
         return {'msg': 'You do not have rights to delete services!', 'status': 403}
-
-    deleted_service = Repository.delete_service(service_id)
-    return deleted_service
