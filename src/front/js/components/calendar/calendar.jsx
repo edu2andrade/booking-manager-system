9259c7ef4f6f9d53316a5ce3f2756cc833c202b6
Enricrypto <eibarraf@gmail.com>
2023-05-16 13:00:23 +0000

    feat: front/calendar

--- src/front/js/components/calendar/calendar.jsx
+++ src/front/js/components/calendar/calendar.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactCalendar from "react-calendar";
 import "../../components/calendar/styles.css";
 
-const Calendar = ({ onChange, value }) => {
+const Calendar = ({ onChange, value, onClickDay }) => {
   return (
     <div>
-      <ReactCalendar onChange={onChange} value={value} />
+      <ReactCalendar
+        onChange={onChange}
+        value={value}
+        onClickDay={onClickDay}
+      />
     </div>
   );
 };
