7734ef101eb503975397c00c479edb19d26b2529
RonaldoDairo <programaciondairo@gmail.com>
2023-05-17 21:49:01 +0000

    feat: Front-End PROFILE

--- src/front/js/pages/userDashboard/index.jsx
+++ src/front/js/pages/userDashboard/index.jsx
@@ -24,20 +24,6 @@ const UserDashboard = () => {
   const handleSubmit = () => {
     navigate(`/profile/${user.id}`);
   };
-  // const [state, setState] = useState({
-  //   id: 0,
-  //   email: "",
-  //   avatar: "",
-  // });
-
-  // const getUser = async () => {
-  //   const data = await obtainInfo();
-  //   console.log("DATA", data);
-  //   setState(data);
-  // };
-
-  // useEffect(() => getUser(), []);
-
   return (
     <div className="jumbotron">
       <h1>User Dashboard</h1>
