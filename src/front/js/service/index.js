fe7dab413a499323eb7021ee68aeefd6dccdeac1
RonaldoDairo <programaciondairo@gmail.com>
2023-05-27 10:39:14 +0000

    resolve conflict

--- src/front/js/service/index.js
+++ src/front/js/service/index.js
@@ -1,6 +1,5 @@
 export const URL = `${process.env.BACKEND_URL}/api`;
-export const URL =
-  "https://3000-edu2andrade-bookingmana-8wzoec2h0y3.ws-eu98.gitpod.io/api";
+
 
 export * as user from "./user";
 export * as service from "./service";
