26ecd197b1edeaaf0290bc91e5e5bb9add449cfc
RonaldoDairo <programaciondairo@gmail.com>
2023-05-27 11:14:06 +0000

    feat: Resolving Styles

--- src/front/js/components/serviceCard/index.jsx
+++ src/front/js/components/serviceCard/index.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import "./styles.css"
+import styles from "./serviceCard.module.css";
 
 export const ServiceCard = ({ services, handleDelete }) => {
   return (
     <>
       {services.map((service) => {
         return !service.is_active ? null : (
-          <div className="list-service" key={service.id}>
-            <div className="text-list-service">
-              <p className="text-servic">{service.name}</p>
-            </div>
-            <button className="btn-list-service me-4">Edit</button>
+          <div className={`${styles._listService}`} key={service.id}>
+            <p className={styles._textService}>{service.name}</p>
+
+            <button className={`${styles._btnListService} me-4 `}>Edit</button>
             <button
-              className="btn-list-service"
+              className={styles._btnListService}
               onClick={() => handleDelete(service.id)}
             >
               Delete
@@ -23,31 +22,3 @@ export const ServiceCard = ({ services, handleDelete }) => {
     </>
   );
 };
-
-
-
-// import React from "react";
-// import styles from './styles.module.css';
-
-// export const ServiceCard = ({ services, handleDelete }) => {
-//   return (
-//     <>
-//       {services.map((service) => {
-//         return !service.is_active ? null : (
-//           <div className={styles.list-service} key={service.id}>
-//             <div className={styles.text-list-service}>
-//               <p className={styles.text-servic}>{service.name}</p>
-//             </div>
-//             <button className={styles.btn-list-service}>Edit</button>
-//             <button
-//               className={styles.btn-list-service}
-//               onClick={() => handleDelete(service.id)}
-//             >
-//               Delete
-//             </button>
-//           </div>
-//         );
-//       })}
-//     </>
-//   );
-// };
