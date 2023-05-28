31ac70d3ffa5d15ec699b45433274de5e94e0d53
Enricrypto <eibarraf@gmail.com>
2023-05-28 11:27:43 +0000

    feat:booking_update

--- src/api/domain/services/route.py
+++ src/api/domain/services/route.py
@@ -40,6 +40,8 @@ def get_single_service(service_id):
     else:
         return Response.response_error(service['msg'], service['status'])
 
+
+
 @api.route('/<int:service_id>', methods=['PATCH'])
 @jwt_required()
 def delete_service(service_id):
