b573a5d330e3beda1d7359873ee9f06f5ca3b167
Eduardo de Souza Andrade <edu2andrade@gmail.com>
2023-06-10 17:23:35 +0200

    fix: console.log deleted on service/user

--- src/front/js/service/user.js
+++ src/front/js/service/user.js
@@ -12,7 +12,6 @@ export const registerUser = async (user) => {
       body: JSON.stringify(user),
     });
     const data = await res.json();
-    console.log(data);
     return data;
   } catch (err) {
     console.log("Error Register User", err);
