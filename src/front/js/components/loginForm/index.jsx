7a900d82f56b06099a3292aff43b2b9c328104f1
RonaldoDairo <programaciondairo@gmail.com>
2023-06-13 19:26:32 +0000

    validation-login

--- src/front/js/components/loginForm/index.jsx
+++ src/front/js/components/loginForm/index.jsx
@@ -5,55 +5,69 @@ import * as Yup from "yup";
 import Button from "../button/index.jsx";
 import styles from "./login.module.css";
 import InputField from "../inputField/index.jsx";
+import { Link } from "react-router-dom";
+import Spinner from "../spinner/index.jsx"
 
-const validationShema = Yup.object().shape({
+
+const validationSchema = Yup.object().shape({
   email: Yup.string()
     .required("Email is required")
     .email("Invalid email format"),
   password: Yup.string()
-  .matches(/^(?=.*\d{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{12,}$/, 'La contraseña no cumple los requisitos')
-  .required('Password is required')
+    .required('Password is required'),
 });
 
 
-const LoginForm = ({ handleChange, handleClick }) => {
+const LoginForm = ({  handleClick, handleChange, invalidDate, loading }) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver(validationShema),
+    resolver: yupResolver(validationSchema),
   });
  
-  const onSubmit = (data) =>{
+  const onSubmit = (data) => {
     handleClick(data);
   };
- 
- 
-  return (
-  <form
-    onSubmit={handleSubmit(onSubmit)}
-    onChange={handleChange}
-    className={styles._form}    
-  >
-    <InputField
-      icon="classNamefa-solid fa-envelope"
-      type="email"
-      placeholder="Email"
-      name="email"
-      register={register}
-      errors={errors}
-    />
-    <InputField
-      icon="fa-solid fa-lock"
-      type="password"
-      placeholder="Password"
-      name="password"
-      register={register}
-      errors={errors}
-    />
-    <Button type="submit" title="Login" />
-  </form>
+
+  return ( 
+    <>
+    {loading ? (<Spinner/>) : (
+      <form
+            onSubmit={handleSubmit(onSubmit)}
+            onChange={handleChange}
+            className={styles._form}    
+          >
+            <InputField
+              icon="fa-solid fa-envelope"
+              type="email"
+              placeholder="Email"
+              name="email"
+              register={register}
+              errors={errors}
+            />
+            <InputField
+              icon="fa-solid fa-lock"
+              type="password"
+              placeholder="Password"
+              name="password"
+              register={register}
+              errors={errors}
+            />
+            <Button type="submit" title="Login" />
+      </form>)}
+  {invalidDate && <div>
+                  <p className={styles._fail}>The data entered is incorrect.</p>
+                  <Link to={"/company-register"} className={styles._registerLink}>
+                    Register like client!
+                  </Link>
+                  <Link to={"/company-register"}className={styles._registerLink}>
+                      Register your company!
+                  </Link>
+                  </div>}
+                  
+  </>
   )
  };
 
