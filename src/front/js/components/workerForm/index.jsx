764515fe574aded6b85f5d1b4e3a30109c34afb0
Enricrypto <eibarraf@gmail.com>
2023-06-20 14:31:26 +0000

    fix: create service

--- src/front/js/components/workerForm/index.jsx
+++ src/front/js/components/workerForm/index.jsx
@@ -6,7 +6,7 @@ import styles from "./workerForm.module.css";
 import Button from "../button/index.jsx";
 import { createWorker } from "../../service/workers.js";
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 import { workerSchema } from "../../validations/workerValidation.js";
 import { toast } from "react-toastify";
 
@@ -20,9 +20,12 @@ const initialState = {
 };
 
 const WorkerForm = ({ textBtn }) => {
-
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    resolver: yupResolver(workerSchema)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(workerSchema),
   });
 
   const [newWorker, setNewWorker] = useState(initialState);
@@ -37,7 +40,7 @@ const WorkerForm = ({ textBtn }) => {
 
   const onSubmit = async () => {
     const resMsg = await createWorker(companyID, newWorker);
-    console.log("resMsg", resMsg)
+    console.log("resMsg", resMsg);
     if (resMsg?.error) {
       toast.error(resMsg?.msg);
     } else {
@@ -47,96 +50,102 @@ const WorkerForm = ({ textBtn }) => {
   };
 
   return (
-      <main className={styles._mainContainer}>
-        <BigContainer>
+    <main className={styles._mainContainer}>
+      <BigContainer>
         <h1>Create Worker</h1>
-            <form
-              className={styles._form}
-              onChange={handleChange}
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <Input
-                icon={<i className="fa-solid fa-circle-user"></i>}
-                type="text"
-                placeholder="Username"
-                name="username"
-                value={newWorker.username}
-                register={register}
-              />
-              {errors?.username && (
-                <small className={styles._fail}>
-                  <i className="fa-solid fa-circle-exclamation"></i> {errors.username?.message}
-                </small>
-              )}
-              <Input
-                icon={<i className="fa-solid fa-circle-user"></i>}
-                type="text"
-                placeholder="Firstname"
-                name="firstname"
-                value={newWorker.firstname}
-                register={register}
-              />
-              {errors?.firstname && (
-                <small className={styles._fail}>
-                  <i className="fa-solid fa-circle-exclamation"></i> {errors.firstname?.message}
-                </small>
-              )}
-              <Input
-                icon={<i className="fa-solid fa-circle-user"></i>}
-                type="text"
-                placeholder="Lastname"
-                name="lastname"
-                value={newWorker.lastname}
-                register={register}
-              />
-              {errors?.lastname && (
-                <small className={styles._fail}>
-                  <i className="fa-solid fa-circle-exclamation"></i> {errors.lastname?.message}
-                </small>
-              )}
-              <Input
-                icon={<i className="fa-solid fa-calendar-days"></i>}
-                type="text"
-                placeholder="Working schedule"
-                name="working_schedule"
-                value={newWorker.working_schedule}
-                register={register}
-              />
-              {errors?.working_schedule && (
-                <small className={styles._fail}>
-                  <i className="fa-solid fa-circle-exclamation"></i> {errors.working_schedule?.message}
-                </small>
-              )}
-              <Input
-                icon={<i className="fa-solid fa-envelope"></i>}
-                type="email"
-                placeholder="email"
-                name="email"
-                value={newWorker.email}
-                register={register}
-              />
-              {errors?.email && (
-                <small className={styles._fail}>
-                  <i className="fa-solid fa-circle-exclamation"></i> {errors.email?.message}
-                </small>
-              )}
-              <Input
-                icon={<i className="fa-solid fa-lock"></i>}
-                type="password"
-                placeholder="password"
-                name="password"
-                value={newWorker.password}
-                register={register}
-              />
-              {errors?.password && (
-                <small className={styles._fail}>
-                  <i className="fa-solid fa-circle-exclamation"></i> {errors.password?.message}
-                </small>
-              )}
-              <Button type="submit" title={textBtn} />
-            </form>
-          </BigContainer>
-      </main>
+        <form
+          className={styles._form}
+          onChange={handleChange}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Input
+            icon={<i className="fa-solid fa-circle-user"></i>}
+            type="text"
+            placeholder="Username"
+            name="username"
+            value={newWorker.username}
+            register={register}
+          />
+          {errors?.username && (
+            <small className={styles._fail}>
+              <i className="fa-solid fa-circle-exclamation"></i>{" "}
+              {errors.username?.message}
+            </small>
+          )}
+          <Input
+            icon={<i className="fa-solid fa-circle-user"></i>}
+            type="text"
+            placeholder="Firstname"
+            name="firstname"
+            value={newWorker.firstname}
+            register={register}
+          />
+          {errors?.firstname && (
+            <small className={styles._fail}>
+              <i className="fa-solid fa-circle-exclamation"></i>{" "}
+              {errors.firstname?.message}
+            </small>
+          )}
+          <Input
+            icon={<i className="fa-solid fa-circle-user"></i>}
+            type="text"
+            placeholder="Lastname"
+            name="lastname"
+            value={newWorker.lastname}
+            register={register}
+          />
+          {errors?.lastname && (
+            <small className={styles._fail}>
+              <i className="fa-solid fa-circle-exclamation"></i>{" "}
+              {errors.lastname?.message}
+            </small>
+          )}
+          <Input
+            icon={<i className="fa-solid fa-calendar-days"></i>}
+            type="text"
+            placeholder="Working schedule"
+            name="working_schedule"
+            value={newWorker.working_schedule}
+            register={register}
+          />
+          {errors?.working_schedule && (
+            <small className={styles._fail}>
+              <i className="fa-solid fa-circle-exclamation"></i>{" "}
+              {errors.working_schedule?.message}
+            </small>
+          )}
+          <Input
+            icon={<i className="fa-solid fa-envelope"></i>}
+            type="email"
+            placeholder="email"
+            name="email"
+            value={newWorker.email}
+            register={register}
+          />
+          {errors?.email && (
+            <small className={styles._fail}>
+              <i className="fa-solid fa-circle-exclamation"></i>{" "}
+              {errors.email?.message}
+            </small>
+          )}
+          <Input
+            icon={<i className="fa-solid fa-lock"></i>}
+            type="password"
+            placeholder="password"
+            name="password"
+            value={newWorker.password}
+            register={register}
+          />
+          {errors?.password && (
+            <small className={styles._fail}>
+              <i className="fa-solid fa-circle-exclamation"></i>{" "}
+              {errors.password?.message}
+            </small>
+          )}
+          <Button type="submit" title={textBtn} />
+        </form>
+      </BigContainer>
+    </main>
   );
 };
 
