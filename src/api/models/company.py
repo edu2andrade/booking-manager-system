5c8ee629250ae578332c2c70533a0de6833f0a75
Enricrypto <eibarraf@gmail.com>
2023-06-27 18:21:56 +0000

    fixes

--- src/api/models/company.py
+++ src/api/models/company.py
@@ -7,22 +7,20 @@ class Company(db.Model):
     name = db.Column(db.String(120), unique=True, nullable=False)
     description = db.Column(db.String(300), nullable=False)
     address = db.Column(db.String(200), nullable=False)
-    opening_time = db.Column(db.String(120), nullable=False)
-    closing_time = db.Column(db.String(120), nullable=False)
+    working_schedule = db.Column(db.String(120), nullable=False)
     is_active = db.Column(db.Boolean(), nullable=False, default=True)
 
     user = db.relationship("User")
     services = db.relationship("Services", back_populates="company")
     workers = db.relationship("Workers", back_populates="company")
 
-    def __init__(self, user_id, cif, name, description, address, opening_time, closing_time):
+    def __init__(self, user_id, cif, name, description, address, working_schedule):
         self.user_id = user_id
         self.cif = cif
         self.name = name
         self.description = description
         self.address = address
-        self.opening_time = opening_time
-        self.closing_time = closing_time
+        self.working_schedule = working_schedule
 
     def serialize(self):
         return {
@@ -32,8 +30,7 @@ class Company(db.Model):
             "name": self.name,
             "description": self.description,
             "address": self.address,
-            "opening_time": self.opening_time, 
-            "closing_time": self.closing_time, 
+            "working_schedule": self.working_schedule, 
             "is_active": self.is_active,
             "services": list(map(lambda service: service.serialize(), self.services)),
             "workers": list(map(lambda worker: worker.serialize(), self.workers)),
--- src/front/js/components/companyForm/index.jsx
+++ src/front/js/components/companyForm/index.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import Input from "../input/index.jsx";
 import styles from "./companyForm.module.css";
 import Button from "../button/index.jsx";
-import TimeSelector from "../timePicker/index.jsx";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { companySchema } from "../../validations/companyValidation.js";
@@ -98,7 +97,6 @@ const CompanyForm = ({ textBtn }) => {
           {errors.working_schedule?.message}
         </small>
       )}
-      <TimeSelector />
       <Button type="submit" title={textBtn} />
     </form>
   );
